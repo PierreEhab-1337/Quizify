4ce1a737def2a816992c3226d7b7abd1a1873a9a
ehab <ehab@DESKTOP-6K3F9V9>
2026-07-08 23:29:00 +0400

    feat: choices feature implemented

--- Back-End/src/controllers/questionController.js
+++ Back-End/src/controllers/questionController.js
@@ -4,15 +4,26 @@ import { QUESTION_TYPES } from "../constants.js";
 export const getAllQuestions = async (req,res) => {
     const result = await db.query(
         `SELECT Q.question_id, Q.description, Q.question_type, U.username,
+
         (SELECT STRING_AGG(T.tag_name, ', ')
         FROM question_tag T
         WHERE T.question_id = Q.question_id) AS tags,
+
         (SELECT STRING_AGG(I.image_path, ', ')
         FROM question_image I
-        WHERE I.question_id = Q.question_id) AS images
+        WHERE I.question_id = Q.question_id) AS images,
+
+        (SELECT json_agg(
+        json_build_object(
+            'choice_number', C.choice_number,
+            'status', C.status,
+            'description', C.description,
+            'image_path', C.image_path
+        ) ORDER BY C.choice_number)
+        FROM choice C WHERE C.question_id = Q.question_id) AS choices
+
         FROM question AS Q
-        LEFT JOIN users AS U ON Q.user_id = U.user_id;
-        ;`
+        LEFT JOIN users AS U ON Q.user_id = U.user_id;`
     );
 
     res.status(200).json({
@@ -27,15 +38,28 @@ export const getQuestion = async (req,res) => {
 
     const result = await db.query(
         `SELECT Q.question_id, Q.description, Q.question_type, U.username,
-            (SELECT STRING_AGG(T.tag_name, ', ')
-             FROM question_tag T
-             WHERE T.question_id = Q.question_id) AS tags,
-            (SELECT STRING_AGG(I.image_path, ', ')
-             FROM question_image I
-             WHERE I.question_id = Q.question_id) AS images
-         FROM question AS Q
-         LEFT JOIN users AS U ON Q.user_id = U.user_id
-         WHERE Q.question_id = $1;`,
+
+        (SELECT STRING_AGG(T.tag_name, ', ')
+            FROM question_tag T
+            WHERE T.question_id = Q.question_id) AS tags,
+
+        (SELECT STRING_AGG(I.image_path, ', ')
+            FROM question_image I
+            WHERE I.question_id = Q.question_id) AS images,
+
+        (SELECT json_agg(
+        json_build_object(
+        'choice_number', C.choice_number,
+        'status', C.status,
+        'description', C.description,
+        'image_path', C.image_path
+        ) ORDER BY C.choice_number)
+        FROM choice C WHERE C.question_id = Q.question_id) AS choices
+
+
+        FROM question AS Q
+        LEFT JOIN users AS U ON Q.user_id = U.user_id
+        WHERE Q.question_id = $1;`,
         [id]
     );
 
@@ -53,9 +77,9 @@ export const getQuestion = async (req,res) => {
 }
 
 export const createQuestion = async (req,res) => {
-    const {description, question_type, tags} = req.body;
+    const {description, question_type, tags, choices} = req.body;
     const user_id = req.user.userId;
-    if(description === undefined || question_type === undefined || !description.trim() )
+    if(description === undefined || question_type === undefined || !description.trim() || choices === undefined)
         return res.status(400).json({
             success : false,
             message: "Fill missing fields!"
@@ -66,6 +90,36 @@ export const createQuestion = async (req,res) => {
             success : false,
             message: "No such Question Type"
         });
+    
+    if((question_type === "singleChoice" || question_type === "multiChoice") && choices.length < 2)
+        return res.status(400).json({
+            success : false,
+            message: "There should be at least 2 choices"
+        });
+    
+    if(question_type === "openEnded" && choices.length > 0)
+        return res.status(400).json({
+            success : false,
+            message: "Open ended questions must have no choices"
+        });
+
+    let correctChoicesCount = 0;
+    if(question_type !== "openEnded"){
+        choices.forEach(choice => {
+            if(choice.status)
+                correctChoicesCount++;
+        });
+        if(!correctChoicesCount)
+            return res.status(400).json({
+            success : false,
+            message: "There must be at least one correct choice"
+        });
+        if(question_type === "singleChoice" && correctChoicesCount > 1)
+            return res.status(400).json({
+            success : false,
+            message: "Single choice questions must have one right choice only"
+        });
+    }
 
     const insertQuestionQuery = await db.query(
         `INSERT INTO question(user_id, description, question_type) VALUES ($1, $2, $3) RETURNING question_id`,
@@ -83,7 +137,18 @@ export const createQuestion = async (req,res) => {
         }
     }
 
-    res.status(200).json({
+    if(question_type !== "openEnded"){
+        let x = 0;
+        for (const choice of choices){
+            x++;
+            await db.query(
+                `INSERT INTO choice(question_id, choice_number, status, description) VALUES ($1, $2, $3, $4)`,
+                [question_id, x, choice.status, choice.description]
+            )
+        }
+    }
+
+    res.status(201).json({
         success : true,
         message: "Question Created Successfully",
         data : question_id
