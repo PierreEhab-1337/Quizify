a358adf3f7cdfc48db6d12e7db08acc9e2a2efd1
ehab <ehab@DESKTOP-6K3F9V9>
2026-07-12 01:24:01 +0400

    constants file

--- Back-End/src/constants.js
+++ Back-End/src/constants.js
@@ -1,2 +1,4 @@
 export const SAFE_USER_FIELDS = "user_id, username, email, role, created_at";
 export const QUESTION_TYPES = ["singleChoice", "multiChoice", "openEnded"];
+export const CONTEST_STATUS = ["completed", "draft", "saved", "inProgress"];
+export const QUESTION_STATUS = ["correct", "wrong", "pending"];
