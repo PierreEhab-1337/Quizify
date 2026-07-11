38642c7552d695e68d3cecc67da6635987effbe9
ehab <ehab@DESKTOP-6K3F9V9>
2026-07-12 01:26:26 +0400

    With Transaction database feature implemented

--- Back-End/src/config/db.js
+++ Back-End/src/config/db.js
@@ -16,6 +16,24 @@ const pool = new Pool({
     }
 });
 
+async function withTransaction(callback) {
+    const client = await pool.connect();
+    try{
+        await client.query("BEGIN");
+        const result = await callback(client);
+        await client.query("COMMIT");
+        return result;
+    }
+    catch(e){
+        await client.query("ROLLBACK");
+        throw e;
+    }
+    finally{
+        client.release();
+    }
+}
+
 export default {
-    query: (text, params) => pool.query(text, params)
+    query: (text, params) => pool.query(text, params),
+    withTransaction
 };
